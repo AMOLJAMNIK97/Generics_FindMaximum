e83f25b018e1899a0686c5ee3c0e63bf8184ef7a
Amol <ajamnik.mech@gmail.com>
2023-01-15 22:59:32 +0600

    [Amol]- Given 3 Integers find the maximum

---
+++ Generics_FindMax/Generics_FindMax/Integers_Max.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_FindMax
+{
+    public class Integers_Max
+    {
+        public int FindMax_Integers(int FirstNum,int SecondNum,int ThirdNum)
+        {
+            if(FirstNum.CompareTo(SecondNum)>0 && FirstNum.CompareTo(ThirdNum)>0||
+                FirstNum.CompareTo(SecondNum)>=0 && FirstNum.CompareTo(ThirdNum)>0 ||
+                FirstNum.CompareTo(SecondNum)>0 && FirstNum.CompareTo(ThirdNum)>=0)
+            {
+                return FirstNum;
+            }
+            if(SecondNum.CompareTo(FirstNum) > 0 && SecondNum.CompareTo(ThirdNum) > 0 ||
+                SecondNum.CompareTo(FirstNum) >= 0 && SecondNum.CompareTo(ThirdNum) > 0 ||
+                SecondNum.CompareTo(FirstNum) > 0 && SecondNum.CompareTo(ThirdNum) >= 0)
+            {
+                return SecondNum;
+            }
+            if(ThirdNum.CompareTo(FirstNum) > 0 && ThirdNum.CompareTo(SecondNum) > 0 ||
+                ThirdNum.CompareTo(FirstNum) >= 0 && ThirdNum.CompareTo(SecondNum) > 0 ||
+                ThirdNum.CompareTo(FirstNum) > 0 && ThirdNum.CompareTo(SecondNum) >= 0)
+            {
+                return ThirdNum;
+            }
+            return 0;
+        }
+    }
+}
---
+++ Generics_FindMax/Generics_FindMax/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_FindMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Integers_Max integers = new Integers_Max();
+            int max=integers.FindMax_Integers(25, 35, 48);
+            Console.WriteLine(max);
+        }
+    }
+}
